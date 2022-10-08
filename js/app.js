8f1fb209524825e96a09313fdf9ab2592ef252b8
Natalia <nkunegina@yandex.ru>
2022-10-08 19:23:05 +0300

    fixed details

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 // document.addEventListener('mousemove', e => {
-//     //Передаем значения текущего положения курсора мыши при движении в HTML для работы с ними в CSS
 //     document.body.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px;`
 // });
 
